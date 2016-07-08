c21b143b1f5553f682bf8fb4ad9bc9682835ec05
panda2134 <smallpanda2134@gmail.com>
2016-07-08 12:12:19 +0800

    Dijkstra...Will write a Floyd-Warshall version this afternoon

--- 2602_dijkstra.cpp
+++ 2602_dijkstra.cpp
@@ -4,53 +4,63 @@
 #include<queue>
 #include<cmath>
 using namespace std;
-struct Point{
+struct Point {
 	int x,y;
 };
+const int INF=0x7fffffff;
 vector<int> Edges[101];
 int n,m,s,t;
 double E[101][101];
 double d[101];
 Point p[101];
-inline int pow2(int i){return i*i;}
-inline bool relax(int t1,int t2){
-	if(d[t2]>d[t1]+E[t1][t2]){
+double dijkstra(int,int);
+inline int pow2(int i) {
+	return i*i;
+}
+inline bool relax(int t1,int t2) {
+	if(d[t2]>d[t1]+E[t1][t2]) {
 		d[t2]=d[t1]+E[t1][t2];
 		return true;
 	}
 	return false;
 }
-void spfa(int);
-int main(){
+class cmp {
+	public:
+		bool operator()(const int & a, const int & b) {
+			return d[a]>d[b];
+		}
+};
+int main() {
 	cin>>n;
-	for(int i=1;i<=n;i++)
+	for(int i=1; i<=n; i++)
 		cin>>p[i].x>>p[i].y;
 	cin>>m;
 	int t1=0,t2=0;
-	for(int i=1;i<=m;i++){
+	for(int i=1; i<=m; i++) {
 		cin>>t1>>t2;
 		E[t1][t2]=E[t2][t1]=sqrt(pow2(p[t1].x-p[t2].x)+pow2(p[t1].y-p[t2].y));
-		Edges[t1].push_back(t2);Edges[t2].push_back(t1);
+		Edges[t1].push_back(t2);
+		Edges[t2].push_back(t1);
 	}
 	cin>>s>>t;
-	spfa(s);
-	printf("%.2f",d[t]);
+	printf("%.2f",dijkstra(s,t));
 	return 0;
 }
-
-void spfa(int s){
-	//init
-	for(int i=1;i<=n;i++)
-		d[i]=999999999.0;
-	d[s]=0;
-	queue<int> q;
-	q.push(s);
-	while(!q.empty()){
-		int t1=q.front(),t2=0;q.pop();
+double dijkstra(int s,int t) {
+	for(int i=1; i<=n; i++)
+		d[i]=INF;
+	d[s]=0.0;//OvO
+	priority_queue<int,vector<int>,cmp> pq;
+	for(int i=1; i<=n; i++)
+		pq.push(i);
+	while(!pq.empty()){
+		int t1=pq.top(),t2=0;pq.pop();
 		for(int i=0;i<Edges[t1].size();i++){
 			t2=Edges[t1][i];
-			if(relax(t1,t2))
-				q.push(t2);
+			if(relax(t1,t2)){
+				pq.push(t2);
+			}
 		}
 	}
+	return d[t];
 }
