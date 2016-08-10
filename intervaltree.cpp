0ffb070a84803d7c5a7e17c3e9243e611463b72b
panda2134 <smallpanda2134@gmail.com>
2016-08-10 09:31:55 +0800

    Well,interval tree is powerful,but it is a little difficult to code. XD Anyway,it works!

--- intervaltree.cpp
+++ intervaltree.cpp
@@ -1,39 +1,36 @@
-//BUGGY!
 #include <iostream>
-#include <cassert>
 #define mid(x,y) ((x+y)>>1)
 using namespace std;
 int n,m,a[100100],tree[500000];
 int build_tree(int o,int l,int r){
-	if(l==r-1) return tree[o]=a[l];
+	if(l==r) return tree[o]=a[l];
 	else
 		return tree[o]=build_tree(o*2,l,mid(l,r))
-					+build_tree(o*2+1,mid(l,r),r);
+					+build_tree(o*2+1,mid(l,r)+1,r);
 }
 int query(int o,int l,int r,int i,int j){
 	if(l>=i && r<=j) return tree[o];
-	int ret=0;
-	if(i<mid(l,r)) ret+=query(o*2,l,mid(l,r),i,mid(l,r));
-	if(j>mid(l,r)) ret+=query(o*2+1,mid(l,r),r,mid(l,r),j);
-	return ret;
+	if(i<=mid(l,r) && j<=mid(l,r)) return query(o*2,l,mid(l,r),i,j);
+	if(i>mid(l,r) && j>mid(l,r)) return query(o*2+1,mid(l,r)+1,r,i,j);
+	if(i<=mid(l,r) && j>mid(l,r)) return query(o*2,l,mid(l,r),i,mid(l,r))+query(o*2+1,mid(l,r)+1,r,mid(l,r)+1,j);
 }
 void modify(int o,int l,int r,int d,int val){
-	if(l==r-1){tree[o]+=val;return;}
-	if(d<mid(l,r)) modify(o*2,l,mid(l,r),d,val);
-	if(d>=mid(l,r)) modify(o*2+1,mid(l,r),r,d,val);
+	if(l==r){tree[o]+=val;return;}
+	if(d<=mid(l,r)) modify(o*2,l,mid(l,r),d,val);
+	else modify(o*2+1,mid(l,r)+1,r,d,val);
 	tree[o]=tree[o*2]+tree[o*2+1];
 }
 int main(){
 	cin>>n;
 	for(int i=1;i<=n;i++) cin>>a[i];
-	build_tree(1,1,n+1);
+	build_tree(1,1,n);
 	cin>>m;
 	for(int i=1;i<=m;i++){
 		int cmd,a,b;cin>>cmd>>a>>b;
 		if(cmd==1)
-			modify(1,1,n+1,a,b);
+			modify(1,1,n,a,b);
 		else
-			cout<<query(1,1,n+1,a,b+1)<<endl;
+			cout<<query(1,1,n,a,b)<<endl;
 	}
 	return 0;
 }
