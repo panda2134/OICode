0a8ca2a34489ae5a10d7c10462130e8963704327
panda2134 <smallpanda2134@gmail.com>
2017-05-17 17:40:44 +0800

    Create UVa1025.cpp

---
+++ UVa1025.cpp
@@ -0,0 +1,13 @@
+//ToBeDone
+#include<cstdio>
+using namespace std;
+inline int readInt(){
+	int f=1,r=0;char c=getchar();
+	while(c<'0'||c>'9'){if(c=='-')f=-1;c=getchar();}
+	while(c>='0' && c<='9'){r=r*10+c-'0';c=getchar();}
+	return f*r;
+}
+int main(){
+	
+	return 0;
+}
