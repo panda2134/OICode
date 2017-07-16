8492c01dc5c548b36b78cfcbea7e05fe106e1fcc
panda2134 <smallpanda2134@gmail.com>
2017-07-16 21:04:06 +0800

    Update and rename gause.cpp to gauss.cpp

--- gauss.cpp
+++ gauss.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#define REP(i,n) for(int i=1;i<=n;i++)
 using namespace std;
 const int MAXN=100;
 typedef double Mat[MAXN+10][MAXN+10];
@@ -6,7 +7,13 @@ inline void Clear(Mat x){memset(x,0,sizeof(Mat));}
 inline void Assign(Mat x,Mat y){memcpy(x,y,sizeof(Mat));}
 
 void gauss_jordan(Mat A,int n){
-	
+	int r;
+	REP(i,MAXN){
+		r=i;
+		for(int j=i+1;j<=MAXN;j++)
+			if(fabs(A[j][i])>fabs(A[i][i])) r=j;
+		
+	}
 }
 int main(){
 	
