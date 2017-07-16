16205dff88b01e4accf6a6d45bc015ca43b21570
panda2134 <smallpanda2134@gmail.com>
2017-07-16 21:00:15 +0800

    Create gause.cpp

---
+++ gause.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int MAXN=100;
+typedef double Mat[MAXN+10][MAXN+10];
+inline void Clear(Mat x){memset(x,0,sizeof(Mat));}
+inline void Assign(Mat x,Mat y){memcpy(x,y,sizeof(Mat));}
+
+void gauss_jordan(Mat A,int n){
+	
+}
+int main(){
+	
+	return 0;
+}
